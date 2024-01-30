69147324f6861a67b251e05c57729cffac88bed0
Bruna Ballerini <brunaballerini@gmail.com>
2024-01-30 15:26:02 -0300

    Criando model Category como ForeignKey de Contact

--- contact/admin.py
+++ contact/admin.py
@@ -12,3 +12,9 @@ class ContactAdmin (admin.ModelAdmin):
     list_per_page = 10
     list_max_show_all = 200
     list_display_links = ('first_name',)
+
+
+@admin.register(models.Category)
+class CategoryAdmin (admin.ModelAdmin):
+    list_display = ('name',)
+    ordering = ('-id',)
