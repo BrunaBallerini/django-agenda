2c67173c8923a40207f0c331e2785bad04bb25dd
Bruna Ballerini <brunaballerini@gmail.com>
2024-01-31 14:09:20 -0300

    Criando template, url e view para contact.html

--- contact/urls.py
+++ contact/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from contact import views
 
-APP_NAME = 'contact'
+# APP_NAME = 'contact'
 
 urlpatterns = [
+    path('<int:contact_id>/', views.contact, name='contact'),
     path('', views.index, name='index'),
 ]
