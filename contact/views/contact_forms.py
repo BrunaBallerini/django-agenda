031f4d09dddc4c6f8582ea6b928a74fe82342ec5
Bruna Ballerini <brunaballerini@gmail.com>
2024-02-20 18:07:10 -0300

    Movendo ContactForms para forms.py, configurando widgets no formulário

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,43 +1,6 @@
-from typing import Any
+from django.shortcuts import render
 
-from django.core.exceptions import ValidationError
-from django.forms import ModelForm
-from django.shortcuts import get_object_or_404, redirect, render
-
-from contact.models import Contact
-
-
-class ContactForm(ModelForm):
-    class Meta:
-        model = Contact
-        fields = (
-            'first_name',
-            'last_name',
-            'phone',
-            'email',
-        )
-
-    def clean(self):
-        cleaned_data = self.cleaned_data
-        print(cleaned_data)
-
-        # self.add_error(
-        #     'first_name',
-        #     ValidationError(
-        #         'Mensagem de erro',
-        #         code='invalid',
-        #     ),
-        # )
-
-        # self.add_error(
-        #     None,
-        #     ValidationError(
-        #         'Mensagem de erro',
-        #         code='invalid',
-        #     ),
-        # )
-
-        return super().clean()
+from contact.forms import ContactForm
 
 
 def create(request):
